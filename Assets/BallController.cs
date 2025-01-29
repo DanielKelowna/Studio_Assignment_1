0d4543058b7ce69f46548e6eeb9049410fca2482
DanielKelowna <dankelowna@gmail.com>
2025-01-28 21:38:55 -0800

    Basic movement

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -11,7 +11,24 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 inputVector = Vector2.zero;
         Debug.Log("Calling the Update method");
-
+        if(Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;
+        }
+        Debug.Log("Resultant Vector: " + inputVector);
     }
 }
